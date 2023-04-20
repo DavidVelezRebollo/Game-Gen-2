214a490f0dc3851a9195efea370d3c3c0937b363
IceHummingBird <laura.garcs.marti@gmail.com>
2023-04-20 20:20:31 +0200

    + Basic UI + Sprites (UI, seed and backgrounds)

--- Assets/SCRIPTS/Components/Menu/Menu.cs
+++ Assets/SCRIPTS/Components/Menu/Menu.cs
@@ -3,5 +3,13 @@ using UnityEngine;
 namespace ANT.Components.Menu {
     public class Menu : MonoBehaviour {
     
+        public void OnStart()
+        {
+            //TODO - Desvacener el menu
+        }
+        public void OnExit()
+        {
+            Application.Quit();
+        }
     }
 }
